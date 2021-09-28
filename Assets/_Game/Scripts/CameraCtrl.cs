0b2c7f5d219176b114c164c28c8e97f7e5ed5cbb
Santiago Sanmartín <saureth001@gmail.com>
2021-09-28 18:35:48 -0500

    [FEAT] Enfoque 1: Cámara se mueve y rota hacia objeto

--- Assets/_Game/Scripts/CameraCtrl.cs
+++ Assets/_Game/Scripts/CameraCtrl.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Transform cameraGoal;
+    public float cameraMovSpeed = 1.0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, cameraGoal.position, Time.deltaTime * cameraMovSpeed);
+        transform.rotation = Quaternion.Lerp (transform.rotation, cameraGoal.rotation, Time.deltaTime*1.0f);
     }
 }
