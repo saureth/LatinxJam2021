98f7940f8c970b093ddc6b400c168213a4fc31e1
Santiago Sanmartín <saureth001@gmail.com>
2021-09-28 18:39:32 -0500

    [FEAT] Inicio enfoque 2

--- Assets/_Game/Scripts/CameraCtrl.cs
+++ Assets/_Game/Scripts/CameraCtrl.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour
 {
-
+    public bool firstApproach;
+    public bool secondApproach;
     public Transform cameraGoal;
     public float cameraMovSpeed = 1.0f;
 
@@ -15,7 +16,9 @@ public class CameraCtrl : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, cameraGoal.position, Time.deltaTime * cameraMovSpeed);
-        transform.rotation = Quaternion.Lerp (transform.rotation, cameraGoal.rotation, Time.deltaTime*1.0f);
+        if(firstApproach){
+            transform.position = Vector3.Lerp(transform.position, cameraGoal.position, Time.deltaTime * cameraMovSpeed);
+            transform.rotation = Quaternion.Lerp (transform.rotation, cameraGoal.rotation, Time.deltaTime*1.0f);
+        }        
     }
 }
