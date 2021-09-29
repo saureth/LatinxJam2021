0738cbfaca63408272823ff01af08464ade87acf
Santiago Sanmartín <saureth001@gmail.com>
2021-09-28 22:18:26 -0500

    [REF] La rotación del jugador ahora sigue al mouse

--- Assets/_Game/Scripts/PlayerMovement.cs
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float movementSpeed = 1f;
     public float rotationSpeed = 1f;
+    public Vector2 rotation;
     public bool canMove = true;
     private float horizontalInput = 0f;
     private float verticalInput = 0f;
@@ -25,8 +26,13 @@ public class PlayerMovement : MonoBehaviour
     void Update(){
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        
         if(!!canMove){
+            rotation.x += Input.GetAxis("Mouse X") * rotationSpeed;
+            rotation.y += Input.GetAxis("Mouse Y") * rotationSpeed;
+            transform.localRotation = Quaternion.Euler(-rotation.y, rotation.x, 0);
             transform.Translate(new Vector3(horizontalInput * rotationSpeed * Time.deltaTime, 0, verticalInput*movementSpeed * Time.deltaTime));
+
         }        
     }
     
