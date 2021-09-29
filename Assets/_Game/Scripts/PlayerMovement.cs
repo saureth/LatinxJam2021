594536ef71702b6ff8d52d4564870a5296cb39a7
Santiago Sanmartín <saureth001@gmail.com>
2021-09-28 19:39:52 -0500

    [FEAT] Enfoque 1: Se crea script de movimiento

---
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    private float horizontalInput = 0f;
+    private float verticalInput = 0f;
+
+    void Update(){
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * (movementSpeed * Time.deltaTime));
+    }
+    
+}
