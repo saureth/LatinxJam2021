629601de999e4ee859663fef87222f0a20b16c4f
Morion <adrian.martinez@udea.edu.co>
2021-10-01 15:57:47 -0500

    avances en la animación del oso

---
+++ Assets/_Game/Scripts/Personalizar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personalizar : MonoBehaviour
+{
+    public Materializador materialPrincipal;
+    
+    [ContextMenu("Cambiar material ppal")]
+    public void CambiarMaterialPrincipal()
+	{
+        materialPrincipal.Siguiente();
+	}
+}
+[System.Serializable]
+public class Materializador
+{
+    public Material[] materiales;
+    public MeshRenderer[] mallas;
+    public int actual;
+
+    public void Siguiente()
+	{
+        actual = (actual + 1) % materiales.Length;
+        ActualizarMateriales();
+	}
+
+    public void ActualizarMateriales()
+	{
+		for (int i = 0; i < mallas.Length; i++)
+		{
+            mallas[i].material = materiales[actual];
+		}
+	}
+}
