f6e2d71ec795072f9a67a1422ba8e45edc86d2f8
Santiago Sanmartín <saureth001@gmail.com>
2021-10-01 15:15:56 -0500

    [FIX] Se resube rama por un error actualizando la escena incorrecta

---
+++ Assets/_Game/Scripts/BookCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookCtrl : MonoBehaviour
+{
+
+    public List<string> texts;
+    public static BookCtrl singleton;
+
+    private void Awake() {
+        if(singleton == null){
+            singleton = this;
+        }
+        else{
+            DestroyImmediate(this.gameObject);
+        }
+        this.texts = new List<string>();
+    }
+
+    public void AddTextToList(string textToAdd){
+        texts.Add(textToAdd);
+    }
+
+}
