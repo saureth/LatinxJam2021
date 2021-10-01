72b9afe8cb318fba4e4651a1ac4ee4ee979a7143
Santiago Sanmartín <saureth001@gmail.com>
2021-10-01 14:38:52 -0500

    [FEAT] Se crea prefab de enemigo con ruta para patrullar

---
+++ Assets/_Game/Scripts/EnemyCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCtrl : MonoBehaviour
+{
+    public Transform A;
+    public Transform B;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(A.position, B.position);    
+    }
+}
