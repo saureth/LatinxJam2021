27e78077888b1da4c105a970f84d60c2ef209060
Morion <adrian.martinez@udea.edu.co>
2021-10-02 12:04:53 -0500

    Inicio de personalización del peluche

--- Assets/_Game/Scripts/Personalizar.cs
+++ Assets/_Game/Scripts/Personalizar.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class Personalizar : MonoBehaviour
 {
     public Materializador materialPrincipal;
-    
+    public Materializador materialOjos;
+    public Materializador materialNariz;
+
     [ContextMenu("Cambiar material ppal")]
     public void CambiarMaterialPrincipal()
-	{
+    {
         materialPrincipal.Siguiente();
-	}
+    }
+    [ContextMenu("Cambiar material Ojos")]
+    public void CambiarMaterialOjos()
+    {
+        materialOjos.Siguiente();
+    }
+    [ContextMenu("Cambiar material Nariz")]
+    public void CambiarMaterialNariz()
+    {
+        materialNariz.Siguiente();
+    }
 }
 [System.Serializable]
 public class Materializador
 {
-    public Material[] materiales;
-    public MeshRenderer[] mallas;
-    public int actual;
+    public Material[]   materiales;
+    public Renderer[]   mallas;
+    public int          actual;
 
     public void Siguiente()
 	{
@@ -32,4 +44,23 @@ public class Materializador
             mallas[i].material = materiales[actual];
 		}
 	}
+}
+
+[System.Serializable]
+public class MallasCambiantes
+{
+    public GameObject[] mallas;
+    public int actual;
+    public void Siguiente()
+    {
+        actual = (actual + 1) % mallas.Length;
+        Actualizar();
+    }
+    public void Actualizar()
+    {
+        for (int i = 0; i < mallas.Length; i++)
+        {
+            mallas[i].SetActive(i == actual);
+        }
+    }
 }
