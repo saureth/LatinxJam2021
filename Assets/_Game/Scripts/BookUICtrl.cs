ca3d67f7ceda9149a3656c48e7cf5c4e3c681ff0
Santiago Sanmartín <saureth001@gmail.com>
2021-10-01 20:03:30 -0500

    [FEAT] Se crea interfaz para libro

---
+++ Assets/_Game/Scripts/BookUICtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookUICtrl : MonoBehaviour
+{
+    public GameObject panel;
+    public Text leftLeaf;
+    public Text rightLeaf;
+    public static BookUICtrl singleton;
+    public List<string> textList;
+    public int phraseAmountPerPage = 3;
+    public int currentPages = 1;
+    int currentLeftPhrases = 0;
+    int currentRightPhrases = 0;
+    string leftLeaftText;
+    string rightLeafText;
+
+    private void Awake() {
+        if(singleton == null){
+            singleton = this;
+        }
+        else{
+            DestroyImmediate(this.gameObject);
+        }
+        this.textList = new List<string>();
+    }
+
+    public void addTextToBook(string text){
+        textList.Add(text);
+    }
+
+    public void ShowBookUI(){
+        this.panel.SetActive(true);
+    }
+}
