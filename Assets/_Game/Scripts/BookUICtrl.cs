6923201989c62a6166f8ef7954e00d2cd24d2f5f
Santiago Sanmartín <saureth001@gmail.com>
2021-10-01 20:39:34 -0500

    [FEAT] Se agrega cálculo de hoja y página para la frase

--- Assets/_Game/Scripts/BookUICtrl.cs
+++ Assets/_Game/Scripts/BookUICtrl.cs
@@ -6,16 +6,16 @@ using UnityEngine.UI;
 public class BookUICtrl : MonoBehaviour
 {
     public GameObject panel;
-    public Text leftLeaf;
-    public Text rightLeaf;
+    public Text leftLeafUI;
+    public Text rightLeafUI;
     public static BookUICtrl singleton;
     public List<string> textList;
     public int phraseAmountPerPage = 3;
-    public int currentPages = 1;
-    int currentLeftPhrases = 0;
-    int currentRightPhrases = 0;
-    string leftLeaftText;
-    string rightLeafText;
+    public int currentPages = 0;
+    public int currentLeftPhrases = 0;
+    public int currentRightPhrases = 0;
+    public string leftLeaftText;
+    public string rightLeafText;
 
     private void Awake() {
         if(singleton == null){
@@ -28,6 +28,29 @@ public class BookUICtrl : MonoBehaviour
     }
 
     public void addTextToBook(string text){
+        //longitud antes es %6 == 0 -> nueva página y escribir en izquierda
+        if(textList.Count % (phraseAmountPerPage *2) == 0){
+            //aumenta páginas
+            currentPages++;
+            leftLeaftText = text;
+            rightLeafText = "";
+            currentLeftPhrases = 1;
+            currentRightPhrases = 0;
+        }
+        // longitud antes %6 != 0 -> calcular izquierda o derecha
+        else {
+            // frases a izquierda %3 ==0 -> añadir a lo ya escrito en derecha 
+            if(currentLeftPhrases % phraseAmountPerPage == 0){
+                currentRightPhrases++;
+                rightLeafText += text;
+            }
+            // frases a izquierda %3 != 0 -> añadir a lo ya escrito en izquierda
+            else {
+                currentLeftPhrases++;
+                leftLeaftText += text;
+            }
+        }
+        // Siempre, al final, añadir texto a la lista
         textList.Add(text);
     }
 
