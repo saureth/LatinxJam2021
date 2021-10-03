d409d282d85e1a03ee90f06578913949146cd58d
Morion <adrian.martinez@udea.edu.co>
2021-10-02 23:33:36 -0500

    Nuevamente texturas

---
+++ Assets/_Game/Scripts/AutoTextureChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoTextureChange : MonoBehaviour
+{
+    public Material[] materiales;
+    void Start()
+    {
+        GetComponent<Renderer>().material = materiales[Random.Range(0, materiales.Length)];
+    }
+}
