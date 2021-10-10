185359ebbf86127e34d11b8ad83bf13d01681fb0
Gabriel <gabrielcamargogarcia@gmail.com>
2021-10-10 03:32:42 -0500

    add enemy movement and swept flashlight logic

--- Assets/_Game/Scripts/EnemyCtrl.cs
+++ Assets/_Game/Scripts/EnemyCtrl.cs
@@ -6,14 +6,34 @@ public class EnemyCtrl : MonoBehaviour
 {
     public Transform A;
     public Transform B;
+    public Transform doll;
+    public Vector3 arrivalPoint;
+    public float speed;
     public static bool congelados = false;
     public Adventure aventura;
 
 	private void Start()
 	{
+        transform.position = A.position;
+        transform.LookAt(B.position);
+        arrivalPoint = B.position;
         aventura.myEvent.AddListener(Activar);
 	}
+    	private void Update()
+	{
+        float distance = Vector3.SqrMagnitude(arrivalPoint - doll.position);
+        
+        doll.Translate(Vector3.forward * speed * Time.deltaTime);
+        if(distance <= .1f)
+        {
+            arrivalPoint = (arrivalPoint==B.position)?A.position:B.position;
+        }
+            Quaternion targetRotation = Quaternion.LookRotation(arrivalPoint - doll.position, Vector3.up);
+            doll.rotation = Quaternion.Slerp(doll.rotation, targetRotation, Time.deltaTime * 3.0f);
+            
+        
 
+    }
     public void Activar()
 	{
 
