35ac23568295687ae560536f587a6d97380c5f31
Morion <adrian.martinez@udea.edu.co>
2021-10-09 19:28:06 -0500

    inicio de la llave y particulas

---
+++ Assets/_Game/Scripts/Rotador.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotador : MonoBehaviour
+{
+    public Vector3 velocidad;
+    void Update()
+    {
+        transform.Rotate(velocidad * Time.deltaTime);
+    }
+}
