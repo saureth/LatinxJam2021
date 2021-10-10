2498bd6238adb3912963e0a638f60b8f9ba75a12
Morion <adrian.martinez@udea.edu.co>
2021-10-10 17:31:21 -0500

    Game over

---
+++ Assets/_Game/Scripts/Escenas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Escenas : MonoBehaviour
+{
+    public void CargarEscena(string nombre)
+	{
+		SceneManager.LoadScene(nombre);
+	}
+}
