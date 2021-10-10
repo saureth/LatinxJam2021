101e0224d87f77c5b003c991c4f47e5b1a999101
Saureth <saureth001@gmail.com>
2021-10-10 17:22:42 -0500

    [FEAT] Script que crea preferencia de lenguaje

---
+++ Assets/_Game/Scripts/LanguageCtrl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageCtrl : MonoBehaviour
+{
+    public void SetLanguage(int lang){
+        PlayerPrefs.SetInt("Lang", lang);
+    }
+}
