34b42cb1f3226a6edd4d53e9ba04d54d7d7d6664
Morion <adrian.martinez@udea.edu.co>
2021-10-10 19:03:36 -0500

    Correcciones de muñecas

--- Assets/_Game/Scripts/EnemyCtrl.cs
+++ Assets/_Game/Scripts/EnemyCtrl.cs
@@ -11,32 +11,57 @@ public class EnemyCtrl : MonoBehaviour
     public float speed;
     public static bool congelados = false;
     public Adventure aventura;
+    public Animator muñecaAnim;
+    public Phrase[] phrases;
 
-	private void Start()
+    float distance;
+
+    private void Start()
 	{
-        transform.position = A.position;
-        transform.LookAt(B.position);
+        doll.position = A.position;
+        doll.LookAt(B.position);
+        aventura.phrase = phrases[Random.Range(0, phrases.Length)];
         arrivalPoint = B.position;
         aventura.myEvent.AddListener(Activar);
+        DeckCtrl.singleton.adventureDeck.Add(aventura);
+        doll.gameObject.SetActive(false);
 	}
-    	private void Update()
-	{
-        float distance = Vector3.SqrMagnitude(arrivalPoint - doll.position);
+    private void Update()
+    {
+        
+		if (congelados || !doll.gameObject.activeSelf)
+		{
+            return;
+		}
+
+        distance = Vector3.SqrMagnitude(arrivalPoint - doll.position);
         
         doll.Translate(Vector3.forward * speed * Time.deltaTime);
         if(distance <= .1f)
         {
             arrivalPoint = (arrivalPoint==B.position)?A.position:B.position;
         }
-            Quaternion targetRotation = Quaternion.LookRotation(arrivalPoint - doll.position, Vector3.up);
-            doll.rotation = Quaternion.Slerp(doll.rotation, targetRotation, Time.deltaTime * 3.0f);
-            
-        
-
+        Quaternion targetRotation = Quaternion.LookRotation(arrivalPoint - doll.position, Vector3.up);
+        doll.rotation = Quaternion.Slerp(doll.rotation, targetRotation, Time.deltaTime * 3.0f);
     }
+
+	private void FixedUpdate()
+	{
+        muñecaAnim.enabled = !congelados; 
+    }
+
+	public static void Congelar()
+    {
+        congelados = true;
+    }
+    public static void Descongelar()
+    {
+        congelados = false;
+    }
+
     public void Activar()
 	{
-
+        doll.gameObject.SetActive(true);
 	}
 
 
--- Assets/_Game/Scripts/EnemySwept.cs
+++ Assets/_Game/Scripts/EnemySwept.cs
@@ -8,16 +8,25 @@ public class EnemySwept : MonoBehaviour
     public float distanciaVision;
     public UnityEvent atacar;
 
+    public static bool invisible;
+
     // Update is called once per frame
     void Update()
     {
+        //if (PlayerMovement.singleton.playerIsInvisible) return;
          RaycastHit hit;
 
-         transform.localEulerAngles = Vector3.up * anguloBarrido * Mathf.Sin(Time.time);
+         transform.localEulerAngles = Vector3.up * anguloBarrido * Mathf.Sin(Time.time*5);
         
         if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distanciaVision))
-            {
-                atacar.Invoke();
-            }
+        {
+            atacar.Invoke();
+        }
     }
+
+	private void OnDrawGizmos()
+	{
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * distanciaVision);
+	}
 }
