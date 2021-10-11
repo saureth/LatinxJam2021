ce06ccfcf15a626167deb6f6cef9162e489e65bb
Alex <inquernon@gmail.com>
2021-10-10 19:54:28 -0500

    objetos aparecen en una distribucion random

---
+++ Assets/_Game/Scripts/Procedural/AutoDesactivarObjetos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDesactivarObjetos : MonoBehaviour
+{
+    public float probActivarObj;
+    // Start is called before the first frame update
+    void Start()
+    {        
+        gameObject.SetActive(Random.Range(0f, 1f) < probActivarObj);
+    }
+}
