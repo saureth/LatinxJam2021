03a4b098c336a7ab5d6ab9ced9cccd6b3092712d
Gabriel <gabrielcamargogarcia@gmail.com>
2021-10-10 20:40:08 -0500

    Add freeze enemies script

---
+++ Assets/_Game/Scripts/FreezeEnemies.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeEnemies : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public IEnumerator Congelar(float segundos)
+    {
+        EnemyCtrl.congelados = true;
+        yield return new WaitForSeconds(segundos);
+        EnemyCtrl.congelados = false;
+    }
+
+    // Update is called once per frame
+    public void StartCongelar(float segundos)
+    {
+        StartCoroutine(Congelar(segundos));
+    }
+}
